822137b52445fca38f3494e85b4d8f19bb01be26
S. M. Ahad Ali Chowdhury <smahadalichowdhury@gmail.com>
2024-12-17 12:42:01 +0600

    first commit

--- app/api/services.ts
+++ app/api/services.ts
@@ -1,6 +1,6 @@
 import { Item, ApiResponse } from '../../types';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/api`;
 
 export const api = {
     // Get all items
